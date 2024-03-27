d686ba2d2eaf41232ae45b0df1c3644daed85e57
Matheus Vinicius <78880370+matheusviniciusbrito@users.noreply.github.com>
2024-03-27 11:06:24 -0300

    Inserção de NOT NULLs

--- ExerciciosAula1/ExercicioUniversidade.sql
+++ ExerciciosAula1/ExercicioUniversidade.sql
@@ -5,47 +5,47 @@ USE db_universidades
 
 CREATE TABLE tbCampus (
 	codCampus INT PRIMARY KEY IDENTITY(1,1)
-	,nomeCampus VARCHAR(200)
-	,logradouro VARCHAR(50)
-	,numero VARCHAR(5)
-	,complemento VARCHAR(20)
-	,cep VARCHAR(9)
-	,bairro VARCHAR(50)
-	,cidade VARCHAR(50)
-	,estado VARCHAR(20)
-	,telefone VARCHAR(14)
+	,nomeCampus VARCHAR(200) NOT NULL
+	,logradouro VARCHAR(50) NOT NULL
+	,numero VARCHAR(5) NOT NULL
+	,complemento VARCHAR(20) NOT NULL
+	,cep VARCHAR(9) NOT NULL
+	,bairro VARCHAR(50) NOT NULL
+	,cidade VARCHAR(50) NOT NULL
+	,estado VARCHAR(20) NOT NULL
+	,telefone VARCHAR(14) NOT NULL
 );
 
 CREATE TABLE tbCurso (
 	codCurso INT PRIMARY KEY IDENTITY(1,1)
-	,nomeCurso VARCHAR(120)
-	,descricao VARCHAR(200)
+	,nomeCurso VARCHAR(120) NOT NULL
+	,descricao VARCHAR(200) NOT NULL
 	,codCampus INT FOREIGN KEY REFERENCES tbCampus(codCampus)
 );
 
 CREATE TABLE tbDisciplina (
 	codDisciplina INT PRIMARY KEY IDENTITY(1,1)
-	,nomeDisciplina VARCHAR(120)
-	,descricao VARCHAR(200)
+	,nomeDisciplina VARCHAR(120) NOT NULL
+	,descricao VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE tbProfessor (
 	codProfessor INT PRIMARY KEY IDENTITY(1,1)
-	,nome VARCHAR(200)
-	,email VARCHAR(100)
+	,nome VARCHAR(200) NOT NULL
+	,email VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE tbAluno (
 	codAluno INT PRIMARY KEY IDENTITY(1,1)
-	,nome VARCHAR(200)
-	,email VARCHAR(100)
+	,nome VARCHAR(200) NOT NULL
+	,email VARCHAR(100) NOT NULL
 )
 
 CREATE TABLE tbTurma (
 	codTurma INT PRIMARY KEY IDENTITY(1,1)
-	,anoFormacao VARCHAR(4)
-	,nomeTurma VARCHAR(50)
-	,numeroSala VARCHAR(4)
+	,anoFormacao VARCHAR(4) NOT NULL
+	,nomeTurma VARCHAR(50) NOT NULL
+	,numeroSala VARCHAR(4) NOT NULL
 	,codCurso INT FOREIGN KEY REFERENCES tbCurso(codCurso)
 );
 
