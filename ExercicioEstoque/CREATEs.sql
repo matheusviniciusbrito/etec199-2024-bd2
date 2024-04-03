c54ce296f71341fb0eb302f4e9dfbc907cbe282d
matheusviniciusbrito <matheus.vinicius.mvrb@gmail.com>
2024-04-03 17:45:35 -0300

    OMG finally worked

--- ExercicioEstoque/CREATEs.sql
+++ ExercicioEstoque/CREATEs.sql
@@ -1,19 +1,8 @@
-CREATE DATABASE	bdEstoque
+CREATE DATABASE	dbEstoque
 GO
 
-USE bdEstoque
+USE dbEstoque
 
- 
-CREATE TABLE tbVenda (
-	codVenda INT IDENTITY (1,1) PRIMARY KEY
-	,dataVenda DATE  NOT NULL
-	,valorTotalVenda FLOAT  NOT NULL
-	,codCliente INT  NOT NULL
- 
-	FOREIGN KEY (codCliente) REFERENCES tb_cliente (codCliente)
-);
- 
- 
 CREATE TABLE tbCliente (
 	codCliente INT IDENTITY (1,1) PRIMARY KEY
 	,nomeCliente VARCHAR (100)
@@ -22,22 +11,26 @@ CREATE TABLE tbCliente (
 	,sexoCliente CHAR (1)
 	,dataNascimentoCliente DATE
 );
- 
-CREATE TABLE tbItensVenda (
-	codItensVenda INT IDENTITY (1,1) PRIMARY KEY
-	,codVenda INT NOT NULL
-	,codProduto INT NOT NULL
-	,quantidadeItensVenda INT NOT NULL
-	,subTotalItensVenda FLOAT NOT NULL
-	,FOREIGN KEY (codVenda) REFERENCES tb_venda (codVenda)
-	,FOREIGN KEY (codProduto) REFERENCES tb_produto (codProduto)
+
+CREATE TABLE tbVenda (
+	codVenda INT IDENTITY (1,1) PRIMARY KEY
+	,dataVenda DATE NOT NULL
+	,valorTotalVenda FLOAT NOT NULL
+	,codCliente INT NOT NULL
+	,FOREIGN KEY (codCliente) REFERENCES tbCliente (codCliente)
 );
  
 CREATE TABLE tbFabricante (
 	codFabricante INT IDENTITY (1,1) PRIMARY KEY
 	,nomeFabricante VARCHAR (50) NOT NULL
 );
- 
+
+CREATE TABLE tbFornecedor (
+	codFornecedor INT IDENTITY (1,1) PRIMARY KEY
+	,nomeFornecedor VARCHAR (50) NOT NULL
+	,contatoFornecedor VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE tbProduto (
 	codProduto INT IDENTITY (1,1) PRIMARY KEY
 	,descricaoProduto VARCHAR (100) NOT NULL
@@ -45,12 +38,16 @@ CREATE TABLE tbProduto (
 	,quantidadeProduto INT NOT NULL
 	,codFabricante INT NOT NULL
 	,codFornecedor INT NOT NULL
-	,FOREIGN KEY (codFabricante) REFERENCES tb_fabricante (codFabricante)
-	,FOREIGN KEY (codFornecedor) REFERENCES tb_fornecedor (codFornecedor)
+	,FOREIGN KEY (codFabricante) REFERENCES tbFabricante (codFabricante)
+	,FOREIGN KEY (codFornecedor) REFERENCES tbFornecedor (codFornecedor)
 );
- 
-CREATE TABLE tbFornecedor (
-	codFornecedor INT IDENTITY (1,1) PRIMARY KEY
-	,nomeFornecedor VARCHAR (50) NOT NULL
-	,contadoFornecedor VARCHAR(50) NOT NULL
+
+CREATE TABLE tbItensVenda (
+	codItensVenda INT IDENTITY (1,1) PRIMARY KEY
+	,codVenda INT NOT NULL
+	,codProduto INT NOT NULL
+	,quantidadeItensVenda INT NOT NULL
+	,subTotalItensVenda FLOAT NOT NULL
+	,FOREIGN KEY (codVenda) REFERENCES tbVenda (codVenda)
+	,FOREIGN KEY (codProduto) REFERENCES tbProduto (codProduto)
 );
