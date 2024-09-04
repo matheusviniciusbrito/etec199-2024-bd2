a10bf438e37317ae3fe72943c81fca396c9e7a5a
matheusviniciusbrito <matheus.vinicius.mvrb@gmail.com>
2024-09-04 11:21:37 -0300

    Ex incompletos

--- AvaliacaoSProcedures/SPs e EXECs.sql
+++ AvaliacaoSProcedures/SPs e EXECs.sql
@@ -3,17 +3,25 @@ GO
 
 --a)
 CREATE PROCEDURE spInsertCategorias
+    @categoria NVARCHAR(100)
 AS
 BEGIN
-	INSERT INTO tbCategoriaProduto(nomeCategoriaProduto) VALUES
-	('Bolo Festa'),
-	('Bolo Simples'),
-	('Torta'),
-	('Salgado');
+    IF NOT EXISTS (SELECT 1 FROM tbCategoriaProduto WHERE nomeCategoriaProduto = @categoria)
+    BEGIN
+        INSERT INTO tbCategoriaProduto(nomeCategoriaProduto) VALUES (@categoria);
+    END
+    ELSE
+    BEGIN
+        PRINT 'Categoria já existe.';
+    END
 END;
-
-EXEC spInsertCategorias;
 GO
+
+
+EXEC spInsertCategorias @categoria = 'Bolo Festa';
+EXEC spInsertCategorias @categoria = 'Bolo Simples';
+EXEC spInsertCategorias @categoria = 'Torta';
+EXEC spInsertCategorias @categoria = 'Salgado';
 
 --b)
 CREATE PROCEDURE spInsertNomeVerifica
@@ -22,18 +30,26 @@ CREATE PROCEDURE spInsertNomeVerifica
     @codCategoriaProduto INT
 AS
 BEGIN
-	IF EXISTS (SELECT 1 FROM tbProduto WHERE nomeProduto like @nomeProduto)
-	BEGIN
-		PRINT ('Produto ja existe no estoque');
-	END
-	ELSE
-	BEGIN
+    -- Verifica se a categoria existe
+    IF NOT EXISTS (SELECT 1 FROM tbCategoriaProduto WHERE codCategoriaProduto = @codCategoriaProduto)
+    BEGIN
+        PRINT ('Categoria não existe.');
+    END
+    -- Verifica se o produto já existe
+    ELSE IF EXISTS (SELECT 1 FROM tbProduto WHERE nomeProduto LIKE @nomeProduto)
+    BEGIN
+        PRINT ('Produto já existe no estoque.');
+    END
+    -- Insere o produto se tudo estiver correto
+    ELSE
+    BEGIN
         INSERT INTO tbProduto (nomeProduto, precokiloProduto, codCategoriaProduto)
         VALUES (@nomeProduto, @precokiloProduto, @codCategoriaProduto);
         
         PRINT ('Produto inserido com sucesso.');
-	END
+    END
 END;
+
 
 EXEC spInsertNomeVerifica 'Bolo Floresta Negra', 42.00, 1;
 EXEC spInsertNomeVerifica 'Bolo Prestígio', 43.00, 1;
@@ -50,34 +66,41 @@ EXEC spInsertNomeVerifica 'Folhado queijo', 31.00, 4;
 GO
 
 --c)
+SELECT * FROM tbCliente
 
 CREATE PROCEDURE spCadastroCliente
-	@nomeCliente VARCHAR(100),
-	@dataNascimentoCliente DATE,
-	@ruaCliente VARCHAR(100),
-	@numCasaCliente INT,
-	@cepCliente VARCHAR(10),
-	@bairroCliente VARCHAR(50),
-	@cpfCliente VARCHAR(11),
-	@sexoCliente CHAR(1)
+    @nomeCliente VARCHAR(100),
+    @dataNascimentoCliente DATE,
+    @ruaCliente VARCHAR(100),
+    @numCasaCliente INT,
+    @cepCliente VARCHAR(10),
+    @bairroCliente VARCHAR(50),
+    @cidadeCliente VARCHAR(50),
+    @estadoCliente CHAR(2),
+    @cpfCliente VARCHAR(11),
+    @sexoCliente CHAR(1)
 AS
 BEGIN
-	IF EXISTS (SELECT cpfCliente FROM tbCliente WHERE cpfCliente LIKE @cpfCliente)
-	BEGIN
-		PRINT ('Cliente cpf '+@cpfCliente+' já cadastrado');
-	END
-	ELSE IF @bairroCliente = 'Guaianases' OR @bairroCliente = 'Itaquera'
-	BEGIN
-		INSERT INTO tbCliente(nomeCliente, dataNascimentoCliente, ruaCliente, numCasaCliente, cepCliente, bairroCliente, cpfCliente, sexoCliente) VALUES
-			(@nomeCliente, @dataNascimentoCliente, @ruaCliente, @numCasaCliente, @cepCliente, @bairroCliente, @cpfCliente, @sexoCliente);
+    -- Verifica se o CPF do cliente já está cadastrado
+    IF EXISTS (SELECT cpfCliente FROM tbCliente WHERE cpfCliente LIKE @cpfCliente)
+    BEGIN
+        PRINT ('Cliente cpf ' + @cpfCliente + ' já cadastrado');
+    END
+    -- Verifica se o bairro é 'Guaianases' ou 'Itaquera'
+    ELSE IF @bairroCliente = 'Guaianases' OR @bairroCliente = 'Itaquera'
+    BEGIN
+        INSERT INTO tbCliente(nomeCliente, dataNascimentoCliente, ruaCliente, numCasaCliente, cepCliente, bairroCliente, cidadeCliente, estadoCliente, cpfCliente, sexoCliente)
+        VALUES (@nomeCliente, @dataNascimentoCliente, @ruaCliente, @numCasaCliente, @cepCliente, @bairroCliente, @cidadeCliente, @estadoCliente, @cpfCliente, @sexoCliente);
 
-			PRINT ('Cliente '+@nomeCliente+' inserido com sucesso')
-	END
-	ELSE
-	BEGIN
-		PRINT ('Não foi possível cadastrar o cliente '+@nomeCliente+' pois o bairro '+@bairroCliente+' não é atendido pela confeitaria');
-	END
+        PRINT ('Cliente ' + @nomeCliente + ' inserido com sucesso');
+    END
+    -- Caso o bairro não seja atendido pela confeitaria
+    ELSE
+    BEGIN
+        PRINT ('Não foi possível cadastrar o cliente ' + @nomeCliente + ' pois o bairro ' + @bairroCliente + ' não é atendido pela confeitaria');
+    END
 END;
+
 
 -- Samira Fatah
 EXEC spCadastroCliente 
@@ -87,6 +110,8 @@ EXEC spCadastroCliente
     @numCasaCliente = 1000,
     @cepCliente = '08.090-000',
     @bairroCliente = 'Guaianases',
+    @cidadeCliente = 'São Paulo',
+    @estadoCliente = 'SP',
     @cpfCliente = '123.456.789-01',
     @sexoCliente = 'F';
 
@@ -98,6 +123,8 @@ EXEC spCadastroCliente
     @numCasaCliente = 234,
     @cepCliente = '08.456-090',
     @bairroCliente = 'Guaianases',
+    @cidadeCliente = 'São Paulo',
+    @estadoCliente = 'SP',
     @cpfCliente = '234.567.890-12',
     @sexoCliente = 'F';
 
@@ -109,6 +136,8 @@ EXEC spCadastroCliente
     @numCasaCliente = 232,
     @cepCliente = '08.109-000',
     @bairroCliente = 'Itaquera',
+    @cidadeCliente = 'São Paulo',
+    @estadoCliente = 'SP',
     @cpfCliente = '345.678.901-23',
     @sexoCliente = 'M';
 
@@ -120,6 +149,8 @@ EXEC spCadastroCliente
     @numCasaCliente = 10,
     @cepCliente = '08.431-090',
     @bairroCliente = 'Guaianases',
+    @cidadeCliente = 'São Paulo',
+    @estadoCliente = 'SP',
     @cpfCliente = '456.789.012-34',
     @sexoCliente = 'M';
 
@@ -131,78 +162,96 @@ EXEC spCadastroCliente
     @numCasaCliente = 300,
     @cepCliente = '08.200-123',
     @bairroCliente = 'Itaquera',
+    @cidadeCliente = 'São Paulo',
+    @estadoCliente = 'SP',
     @cpfCliente = '567.890.123-45',
     @sexoCliente = 'F';
 GO
 
+
 --d)
+select * from tbCliente
+
+SELECT * FROM tbEncomenda
+
 CREATE PROCEDURE spCriarEncomenda
-	@cpfCliente VARCHAR(11),
-	@dataEncomenda DATE,
-	@codCliente INT,
-	@valorTotalEncomenda DECIMAL(10, 2),
-	@dataEntregaEncomenda DATE
+    @cpfCliente VARCHAR(11),
+    @dataEncomenda DATE,
+    @valorTotalEncomenda DECIMAL(10, 2),
+    @dataEntregaEncomenda DATE
 AS
 BEGIN
-	--pegar nome do cliente
-	DECLARE @nomeCliente VARCHAR(100);
-	SET @nomeCliente = (SELECT nomeCliente FROM tbCliente WHERE codCliente = @codCliente);
+    -- Verificar se o cliente está cadastrado e obter o codCliente e nomeCliente
+    DECLARE @codCliente INT;
+    DECLARE @nomeCliente VARCHAR(100);
 
-	IF NOT EXISTS (SELECT 1 FROM tbCliente WHERE cpfCliente LIKE @cpfCliente)
-	BEGIN
-		PRINT('Não foi possível efetivar a encomenda pois o cliente '+@nomeCliente+' não está cadastrado');
-	END;
-	IF @dataEncomenda > @dataEntregaEncomenda
-	BEGIN
-		PRINT('Não é possível entregar uma encomenda antes da encomenda ser realizada')
-	END;
-	INSERT INTO tbEncomenda(dataEncomenda, codCliente, valorTotalEncomenda, dataEntregaEncomenda) VALUES
-		(@dataEncomenda, @codCliente, @valorTotalEncomenda, @dataEntregaEncomenda)
-		
-	--pegar cod da encomenda
-	DECLARE @codEncomenda INT;
-	SET @codEncomenda = SCOPE_IDENTITY();
+    SELECT @codCliente = codCliente, @nomeCliente = nomeCliente 
+    FROM tbCliente 
+    WHERE cpfCliente = @cpfCliente;
 
-	PRINT('Encomenda '+@codEncomenda+' para o cliente ' +@nomeCliente+' efetuada com sucesso');
+    IF @codCliente IS NULL
+    BEGIN
+        PRINT('Não foi possível efetivar a encomenda pois o cliente com CPF ' + @cpfCliente + ' não está cadastrado');
+        RETURN;
+    END
+
+    -- Verificar se a data de entrega não é menor do que a data da encomenda
+    IF @dataEntregaEncomenda < @dataEncomenda
+    BEGIN
+        PRINT('Não é possível entregar uma encomenda antes da encomenda ser realizada');
+        RETURN;
+    END
+
+    -- Inserir a encomenda
+    INSERT INTO tbEncomenda(dataEncomenda, codCliente, valorTotalEncomenda, dataEntregaEncomenda)
+    VALUES (@dataEncomenda, @codCliente, @valorTotalEncomenda, @dataEntregaEncomenda);
+    
+    -- Obter o número da encomenda recém-criada
+    DECLARE @codEncomenda INT;
+    SET @codEncomenda = SCOPE_IDENTITY();
+
+    -- Exibir mensagem de sucesso
+	PRINT('Encomenda ' + CAST(@codEncomenda AS VARCHAR(10)) + ' para o cliente ' + @nomeCliente + ' efetuada com sucesso');
 END;
+GO
 
+SELECT * FROM tbEncomenda
 -- Encomenda 1
 EXEC spCriarEncomenda
-    @cpfCliente = '123.456.789-01',
+    @cpfCliente = '123.456.789',
     @dataEncomenda = '2015-08-08',
-    @codCliente = 1,
     @valorTotalEncomenda = 450.00,
     @dataEntregaEncomenda = '2015-08-15';
 
+
 -- Encomenda 2
 EXEC spCriarEncomenda
-    @cpfCliente = '234.567.890-12',
+    @cpfCliente = '234.567.890', -- Verifique se este CPF corresponde ao codCliente = 2
     @dataEncomenda = '2015-10-10',
-    @codCliente = 2,
     @valorTotalEncomenda = 200.00,
     @dataEntregaEncomenda = '2015-10-15';
 
 -- Encomenda 3
 EXEC spCriarEncomenda
-    @cpfCliente = '345.678.901-23',
+    @cpfCliente = '345.678.901', -- Verifique se este CPF corresponde ao codCliente = 3
     @dataEncomenda = '2015-10-10',
-    @codCliente = 3,
     @valorTotalEncomenda = 150.00,
     @dataEntregaEncomenda = '2015-12-10';
 
 -- Encomenda 4
 EXEC spCriarEncomenda
-    @cpfCliente = '123.456.789-01',
+    @cpfCliente = '123.456.789', -- Correspondente ao codCliente = 1
     @dataEncomenda = '2015-10-06',
-    @codCliente = 1,
     @valorTotalEncomenda = 250.00,
     @dataEntregaEncomenda = '2015-10-12';
 
 -- Encomenda 5
 EXEC spCriarEncomenda
-    @cpfCliente = '456.789.012-34',
+    @cpfCliente = '456.789.012', -- Verifique se este CPF corresponde ao codCliente = 4
     @dataEncomenda = '2015-10-05',
-    @codCliente = 4,
     @valorTotalEncomenda = 150.00,
     @dataEntregaEncomenda = '2015-10-12';
 
+--Arrumar cpf do ex D
+
+--e)
